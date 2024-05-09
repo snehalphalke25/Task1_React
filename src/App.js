e3c5cd28009ad65ec9a3d88ae9982db0aadb96ed
snehalphalke25 <snehal@rasheedfoundation.org>
2024-05-09 19:25:51 +0530

    react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'; 
+import Hello from './Hello'; 
+
+function App() {
+  return (
+    <div className="App">
+      <Hello /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Hello.js
@@ -0,0 +1,5 @@
+import React from 'react'; 
+function Hello() { 
+return <p>Hello, world!</p>; 
+} 
+export default Hello;
